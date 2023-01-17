fa0d550c4af81c64ee1ca09a73b5963ae08f97c5
labeneko <labecat@gmail.com>
2023-01-17 20:28:33 +0900

    授業中の作業内容を追加

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    context: __dirname + '/app',
+    entry: './entry',
+    output: {
+      path: __dirname + '/public/javascripts',
+      filename: 'bundle.js'
+    },
+    mode: 'none',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    resolve: {
+      fallback: {
+        buffer: require.resolve('buffer'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify')
+      }
+    }
+  }
